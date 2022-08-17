23d143224e8bc08cdd0872ab06ec699eaa7bfa7c
kbeloin <kyle.beloin@me.com>
2022-08-16 20:47:09 -0700

    Added twitter - removed photo.

--- src/data/global.tsx
+++ src/data/global.tsx
@@ -2,4 +2,5 @@ export const contact = {
   email: "mailto:me@kylebeloin.com",
   linkedin: "https://www.linkedin.com/in/kbeloin/",
   github: "https://github.com/kbeloin",
+  twitter: "https://twitter.com/kylebeloin",
 };
