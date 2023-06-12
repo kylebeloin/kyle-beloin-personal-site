cef1d67a6002e30d609bc517afd9c355f44ba7f5
kbeloin <kyle.beloin@me.com>
2023-06-12 13:58:49 -0400

    Edited about me.

--- src/data/global.tsx
+++ src/data/global.tsx
@@ -1,6 +1,6 @@
 export const contact = {
   email: "mailto:me@kylebeloin.com",
-  linkedin: "https://www.linkedin.com/in/kylebeloin/",
+  // linkedin: "https://www.linkedin.com/in/kylebeloin/",
   github: "https://github.com/kylebeloin",
-  twitter: "https://twitter.com/kylebeloin",
+  // twitter: "https://twitter.com/kylebeloin",
 };
--- src/data/home.tsx
+++ src/data/home.tsx
@@ -3,40 +3,30 @@ export const intro = {
   description: "intro",
   content: (
     <>
-      <p>Thank you for visiting my website.</p>
       <p>
-        Currently, I'm a frontend developer at{" "}
-        <a
-          style={{ color: "var(--font-color)" }}
-          href={"https://www.deckers.com/"}
-          target={"_blank"}
-          rel="noreferrer"
-        >
-          Deckers Brands
-        </a>
-        , with a degree in Applied Computing and Informatics from the University
-        of Arizona (Fall 2021). As a tech transplant with backgrounds in
-        Philosophy (BA, 2013) and English Literature (MA, 2018), I love
-        learning&mdash;plain and simple; but web development ultimately stole my
-        heart.
+        A frontend developer with a degree in Applied Computing and Informatics
+        from the University of Arizona (Fall 2021). As a tech transplant with
+        backgrounds in Philosophy (BA, 2013) and English Literature (MA, 2018),
+        I love learning&mdash;plain and simple; but web development ultimately
+        stole my heart.
       </p>
     </>
   ),
 };
 
 export const projects = [
-  {
-    title: "Learning Platform",
-    content: (
-      <p>
-        A learning management system that allows teachers to create courses and
-        assign them to students. The system can also be used to administer data
-        collection and analysis.
-      </p>
-    ),
-    link: "https://experiment-builder-demo.herokuapp.com/",
-    tech: ["react", "redux", "python", "django"],
-  },
+  // {
+  //   title: "Learning Platform",
+  //   content: (
+  //     <p>
+  //       A learning management system that allows teachers to create courses and
+  //       assign them to students. The system can also be used to administer data
+  //       collection and analysis.
+  //     </p>
+  //   ),
+  //   link: "https://experiment-builder-demo.herokuapp.com/",
+  //   tech: ["react", "redux", "python", "django"],
+  // },
   {
     title: "Game and Visualizations",
     content: (
@@ -67,17 +57,8 @@ export const about = {
   description: "about",
   content: (
     <p>
-      Currently, I'm a frontend developer at{" "}
-      <a
-        style={{ color: "var(--font-color)" }}
-        href={"https://www.deckers.com/"}
-        target={"_blank"}
-        rel="noreferrer"
-      >
-        Deckers Brands
-      </a>
-      , with a degree in Applied Computing and Informatics from the University
-      of Arizona (Fall 2021).
+      Currently, I'm a frontend developer with a degree in Applied Computing and
+      Informatics from the University of Arizona (Fall 2021).
       <br />
       <br />
       As a tech transplant with backgrounds in Philosophy (BA, 2013) and English
